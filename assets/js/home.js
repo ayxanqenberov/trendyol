ba184f7126ed6ec5ddb178cf2f07c4c4ef27416b
098100ayxan <ganbarov008@gmail.com>
2024-09-10 14:32:55 +0400

    done

---
+++ assets/js/home.js
@@ -0,0 +1,12 @@
+
+// const carousel = document.getElementsByClassName("carousel-sponsor")
+// const prev = document.getElementsByClassName("leftArrow")
+// const next = document.getElementsByClassName("rightArrow")
+
+// const productImg = document.getElementsByClassName("up-part-img")
+// function getFind(){
+//     axios.get("https://fakestoreapi.com/products")
+//     .then((res =>{
+
+//     }))
+// }
